322b03a87f996c7edfddb8a5812457000a5357f5
Hafiz Hamza <hamzamughal11@hotmail.com>
2020-12-09 16:15:56 +0500

    - Remove unnecessary bean initialization - Optimize imports

--- document-service/src/main/java/com/filecloud/documentservice/configuration/DocumentServiceConfiguration.java
+++ document-service/src/main/java/com/filecloud/documentservice/configuration/DocumentServiceConfiguration.java
@@ -1,6 +1,5 @@
 package com.filecloud.documentservice.configuration;
 
-import com.filecloud.documentservice.properties.DocumentServiceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -8,26 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import com.filecloud.documentservice.properties.DocumentServiceProperties;
+
 
 @Configuration
 @EnableConfigurationProperties({
-        DocumentServiceProperties.class
+		DocumentServiceProperties.class
 })
 public class DocumentServiceConfiguration {
 
-    @LoadBalanced
-    @Bean
-    @Primary
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@LoadBalanced
+	@Bean
+	@Primary
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
-//    @Bean
-//    public Validator getValidator() {
-//        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-//        return factory.getValidator();
-//    }
 }
