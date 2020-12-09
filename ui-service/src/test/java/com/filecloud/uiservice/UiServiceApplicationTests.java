d32200cfa95f2e868e9111436c3d6393735bbe5c
Hafiz Hamza <hamzamughal11@hotmail.com>
2020-12-09 16:13:33 +0500

    - Initial UI Service added

---
+++ ui-service/src/test/java/com/filecloud/uiservice/UiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filecloud.uiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
