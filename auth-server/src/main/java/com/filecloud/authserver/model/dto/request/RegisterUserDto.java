01a771f830abc424c516fa75ed4b252cac6bc4ab
Hafiz Hamza <hamzamughal11@hotmail.com>
2020-12-26 12:26:38 +0500

    - Add validation to register user dto

--- auth-server/src/main/java/com/filecloud/authserver/model/dto/request/RegisterUserDto.java
+++ auth-server/src/main/java/com/filecloud/authserver/model/dto/request/RegisterUserDto.java
@@ -1,17 +1,29 @@
 
 package com.filecloud.authserver.model.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterUserDto {
 
-    private String fullName;
-    private String email;
-    private String password;
+	@NotEmpty(message = "Name cannot be empty")
+	@NotNull(message = "Name cannot be empty")
+	private String fullName;
+
+	@Email(message = "Invalid email")
+	private String email;
+
+	@NotEmpty(message = "Password cannot be empty")
+	@NotNull(message = "Password cannot be empty")
+	private String password;
 
 }
